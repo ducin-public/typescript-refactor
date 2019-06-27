596c5114c4aaff8dca8961f0412fa80b8739d285
Kedra, Krystian <krystian.kedra@atos.net>
2019-06-27 13:57:11 +0200

    updated http and created interface for restApi and localeStorage

--- src/model/http.ts
+++ src/model/http.ts
@@ -2,31 +2,14 @@ import axios from 'axios'
 import { BASE_URL, TODOS_URL } from '../../src/model/configuration'
 import { Todo } from '../../src/model/interfaces'
 
-export const getTodos = async () => {
-    const response = await axios.get(`${BASE_URL}/${TODOS_URL}`)
-    return response.data
-}
+export const getTodos = () =>
+    axios.get(`${BASE_URL}/${TODOS_URL}`)
 
-export const createTodo = async (payload: Todo) => {
-    try {
-        await axios.post(`${BASE_URL}/${TODOS_URL}`, payload)
-    } catch (e) {
-        console.error(e)
-    }
-}
+export const createTodo = (payload: Todo) =>
+    axios.post(`${BASE_URL}/${TODOS_URL}`, payload)
 
-export const updateTodo = async (payload: Todo) => {
-    try {
-        await axios.put(`${BASE_URL}/${TODOS_URL}`, payload)
-    } catch (e) {
-        console.error(e)
-    }
-}
+export const updateTodo = (payload: Todo) =>
+    axios.put(`${BASE_URL}/${TODOS_URL}`, payload)
 
-export const deleteTodo = async (payload: Todo['id']) => {
-    try {
-        await axios.delete(`${BASE_URL}/${TODOS_URL}/${payload}`)
-    } catch (e) {
-        console.error(e)
-    }
-}
+export const deleteTodo = (payload: Todo['id']) =>
+    axios.delete(`${BASE_URL}/${TODOS_URL}/${payload}`)
--- src/model/interfaces.ts
+++ src/model/interfaces.ts
@@ -1,5 +1,15 @@
 export interface Todo {
-    id: String,
-    title: String,
-    completed: Boolean
+    id: string,
+    title: string,
+    completed: boolean
+}
+
+export interface Storage {
+    getAll(): Todo[],
+    create(payload: Todo): void,
+    toggle(payload: Todo): void,
+    toggleAll(payload: Todo[]): void,
+    delete(payload: Todo['id']): void,
+    deleteAll(payload: Todo[]): void,
+    update(payload: Todo['title']): void
 }
