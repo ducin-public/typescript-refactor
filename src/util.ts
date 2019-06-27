489b14cc9c84eaca0ba8506f88f2c36d62525424
Marcin Szymainda <marcin.szymainda@atos.net>
2019-06-27 15:51:54 +0200

    refactor: Fixed errors

--- src/util.ts
+++ src/util.ts
@@ -26,7 +26,7 @@ export var util = {
             title
 
     }),
-    store: (namespace, data)=> {
+    store: function (namespace, data?){
         if (arguments.length > 1) {
             return localStorage.setItem(namespace, JSON.stringify(data));
         } else {
